fbb5c931786d057be0bbf78eafd0e8d1062d5ea3
ZoomingLightningbugGirl <sophiepasquarella@gmail.com>
2026-02-22 01:04:20 -0500

    Learned some JavaScript and applied it

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function openPurse() {
+    document.getElementById('MyPurse').alt = "open purse";
+    // add in the images later
+}
+
+// add a function that closes the purse later if it's opened and clicked again.
+// add a function that allows you to zoom in to each item in the purse when clicked
